ab6e0a0ef884dd5545c8d597bfaf42dce1c0344f
Wolkodlakus <lesko68@list.ru>
2022-01-11 17:26:58 +0300

    clarified the use of null and blank model parameters

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,10 +4,10 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     """Покемон"""
     title = models.CharField(verbose_name='Название по русски', max_length=200)
-    title_en = models.CharField(verbose_name='Название по английски',max_length=200, default='', blank=True, null=True)
-    title_jp = models.CharField(verbose_name='Название по японски',max_length=200, default='', blank=True, null=True)
-    image = models.ImageField(verbose_name='Изображение', blank=True,null=True)
-    description = models.TextField(verbose_name='Описание', default='', blank=True, null=True)
+    title_en = models.CharField(verbose_name='Название по английски', max_length=200, default='', blank=True)
+    title_jp = models.CharField(verbose_name='Название по японски', max_length=200, default='', blank=True)
+    image = models.ImageField(verbose_name='Изображение', blank=True, null=True)
+    description = models.TextField(verbose_name='Описание', default='', blank=True)
     evolved_from = models.ForeignKey('self', verbose_name='Из кого эволюционировал',
                                      blank=True, on_delete=models.SET_NULL, null=True,
                                      related_name='evolves_into')
@@ -25,8 +25,8 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
-    appeared_at = models.DateTimeField(verbose_name='Время появление', blank=True, null=True)
-    disappeared_at = models.DateTimeField(verbose_name='Время исчезновения', blank=True, null=True)
+    appeared_at = models.DateTimeField(verbose_name='Время появление', null=True)
+    disappeared_at = models.DateTimeField(verbose_name='Время исчезновения', null=True)
     level = models.IntegerField(verbose_name='Уровень', default=0, blank=True, null=True)
     health = models.IntegerField(verbose_name='Здоровье', default=0, blank=True, null=True)
     strength = models.IntegerField(verbose_name='Сила', default=0, blank=True, null=True)
